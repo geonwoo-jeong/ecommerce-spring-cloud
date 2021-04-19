52a142b183e6bb3a165e74ea95060a126ffa625a
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2021-04-19 22:12:50 +0900

    User Microservice JPA2

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -3,9 +3,12 @@ package com.example.userservice.controller;
 import com.example.userservice.dto.UserDto;
 import com.example.userservice.service.UserService;
 import com.example.userservice.vo.RequestUser;
+import com.example.userservice.vo.ResponseUser;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,13 +29,15 @@ public class UserController {
   }
 
   @PostMapping("/users")
-  public String createUser(@RequestBody RequestUser user) {
+  public ResponseEntity<ResponseUser> createUser(@RequestBody RequestUser user) {
     ModelMapper mapper = new ModelMapper();
     mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
     UserDto userDto = mapper.map(user, UserDto.class);
     userService.createUser(userDto);
 
-    return "Created user method is called";
+    ResponseUser responseUser = mapper.map(userDto, ResponseUser.class);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(responseUser);
   }
 }
