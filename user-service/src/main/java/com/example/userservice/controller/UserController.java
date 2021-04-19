59ad18c69385e833b129b38b45bfa5f68966915e
Geonwoo Jeong <geonwoo.jeong@gmail.com>
2021-04-19 22:50:18 +0900

    User Microservice and Connect Spring Cloud Gateway

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -7,6 +7,7 @@ import com.example.userservice.vo.ResponseUser;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,16 +17,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/user-service")
 @RequiredArgsConstructor
 public class UserController {
 
+  private final Environment environment;
   private final UserService userService;
 
 
   @GetMapping("/health_check")
   public String status() {
-    return "It's Working in User Service";
+    return String.format("It's Working in User Service on PORT %s",
+            environment.getProperty("local.server.port"));
   }
 
   @PostMapping("/users")
